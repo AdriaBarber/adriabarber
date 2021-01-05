dcec8dc2021410af2573710a56e59b3dfd55b1c8
AdriaBarber <76945929+AdriaBarber@users.noreply.github.com>
2021-01-05 01:03:51 +0100

    Delete cn.php

--- cn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost,"root", "","pagina_web");
-if (!$conexion) {
-  echo 'Error al conectar a la base de datos';
-}
-else {
-  echo 'Conectado al abse de datos';
-}
