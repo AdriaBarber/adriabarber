e7d94653dbaa94c19d54c5164a9c88c043200060
AdriaBarber <adrbarberesbert@gmail.com>
2021-01-06 22:42:02 +0100

    Delete contador.php

--- contador.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_COOKIE['contador'])){
-  setcookie('contador', $_COOKIE['contador']+1, time()+2628000);
-  echo "Número de visitas: ".$_COOKIE['contador'];
-}else {
-  setcookie('contador', 1, time()+2628000);
-  echo "Bienvenido por primera vez a nuestra página web";
-}
-?>
