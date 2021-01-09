8fb3ff0fec45fca96bae89850e869c1f564ed621
AdriaBarber <adrbarberesbert@gmail.com>
2021-01-09 22:21:53 +0100

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,13 @@
+<?php
+  if(isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $contraseña = $_POST['contraseña'] . $_POST['c-contraseña'];
+  }
+
+
+
+
+
+
+?>
