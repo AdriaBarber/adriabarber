e266a3ccec00b69f7eeb4b8d8944c59188c019dd
AdriaBarber <76945929+AdriaBarber@users.noreply.github.com>
2021-04-09 16:08:02 +0200

    Delete enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $destino= "adriabarberesbert@gmail.com";
-  $nombre = $_POST["username"];
-  $correo = $_POST["useremail"];
-  $mensaje = $_POST["usermessage"];
-  $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
-  mail($destino,"Contacto", $contenido);
-  header("Location:gracias.html");
-?>
